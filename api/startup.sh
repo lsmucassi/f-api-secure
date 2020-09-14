7d554649c1387f13458906df1ca81bef72a781c1
lsmucassi <lindasmucassi@gmail.com>
2020-09-14 21:31:19 +0200

    final documentation xnl

--- api/startup.sh
+++ api/startup.sh
@@ -2,10 +2,10 @@
 
 python -m venv ../venv;
 source ../venv/bin/activate;
-# pip install django;
-# pip install djangorestframework
-# pip install validator-collection;
-# pip install decorators
+pip install django;
+pip install djangorestframework
+pip install validator-collection;
+pip install decorators
 python manage.py makemigrations;
 python manage.py migrate;
-python manage.py runserve;r
+python manage.py runserve;
